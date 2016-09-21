c7e192d2abf6129f85f4b63b5312f3495e1baa8a
victor fernandes <victor2fernandes@yahoo.com>
2016-09-22 00:35:26 +0530

    changes

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const server = express()
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.on('disconnect', () => console.log('Client disconnected'));
+//    console.log('Client connected');
+//    socket.on('disconnect', () => console.log('Client disconnected'));
 
     socket.on('chat message', function (msg) {
         io.emit('chat message', msg);
