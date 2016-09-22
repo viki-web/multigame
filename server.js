bbc7db81f703d7916d7be42ae69682107cd857a0
victor fernandes <victor2fernandes@yahoo.com>
2016-09-23 01:54:19 +0530

    structure changed

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
-'use strict';
+var express = require('express');
+var socketIO = require('socket.io');
+var path = require('path');
+var app = express();
+var http = require('http');
+var PORT = process.env.PORT || 3000;
+var server = http.createServer(app)
+var io = require('socket.io').listen(server);
 
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
-
-const server = express()
-    .use((req, res) => res.sendFile(INDEX))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-const io = socketIO(server);
+server.listen(PORT, function () {
+    console.log('Express server listening on port %d in %s mode', PORT);
+});
 
 io.on('connection', (socket) => {
-//    console.log('Client connected');
-//    socket.on('disconnect', () => console.log('Client disconnected'));
+    console.log('Client connected');
+    socket.on('disconnect', () => console.log('Client disconnected'));
 
     socket.on('chat message', function (msg) {
         io.emit('chat message', msg);
     });
 });
-
-//setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
